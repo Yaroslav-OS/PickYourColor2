17a80281a718b024b2165870f801c14f8cd486b5
Yaroslav <yaroslav.olex@gmail.com>
2020-12-14 10:00:19 +0200

    Initial Commit

---
+++ PickYourColor2/PickYourColor2App.swift
@@ -0,0 +1,17 @@
+//
+//  PickYourColor2App.swift
+//  PickYourColor2
+//
+//  Created by Yaroslav on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PickYourColor2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
